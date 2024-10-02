aa21f5bba9a0e92244de69657d5e1c7e70b307cc
double T <tangy54tondeur69@gmail.com>
2024-10-02 21:12:12 +0200

    button

--- app/page.tsx
+++ app/page.tsx
@@ -2,52 +2,40 @@ import Card from "@/components/card/Card";
 import Navigation from "@/components/nav/navigation";
 import { fetchPages } from "@/lib/notion";
 import Image from "next/image";
+import Button from "@/components/Button/Button";
 
 export default async function Home() {
   const post = await fetchPages();
 
   return (
-    <main>
+    <>
       <Navigation />
       <header className="h-[600px] w-full azerty flex items-center justify-center flex-col text-3xl  text-white gap-10 ">
         <h1 className="text-6xl font-black">SOUNDBOX MTF</h1>
         <h2>seulement pour les mecs COOL</h2>
       </header>
       <div className="px-24">
-      <ul  className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"> 
+        <ul className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
 
-      {post.results.map((i) => (
-        <Card
-          key={i.id}
-          name={i.properties.Nom.title[0].plain_text}
-          image={i.properties.image.files[0]?.file.url}
-          sound={i.properties.son.files[0]?.file.url}
-        />
-      ))}
+          {post.results.map((i) => (
+            <Card
+              key={i.id}
+              name={i.properties.Nom.title[0].plain_text}
+              image={i.properties.image.files[0]?.file.url}
+              sound={i.properties.son.files[0]?.file.url}
+            />
+          ))}
 
-
-
-
-
-
-</ul>
-</div>
-      {/* {post.results.map((i) => (
-        <div>
-          <h1 key={i.id}>{i.properties.Nom.title[0].plain_text}</h1>
-          <audio
-            controls
-            src={i.properties.son.files[0]?.file.url}
-            type="audio/mpeg"
-          />
-          <Image
-            src={i.properties.image.files[0]?.file.url}
-            alt="azerty"
-            width={300}
-            height={300}
-          />
-        </div>
-      ))} */}
-    </main>
+        </ul>
+      </div>
+    </>
   );
 }
+
+
+
+
+
+
+
+
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,17 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
-
+import { PlayIcon, PauseIcon } from "@heroicons/react/20/solid";
+import Button from '../Button/Button';
 const Card = ({ name, image, sound}) => {
   return (
     <li className="flex flex-col items-center justify-center gap-5 rounded-2xl bg-gray-800 px-8 py-10">
       <Image className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={image} alt=""height={100} width={100} />
       <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">{name}</h3>
-      <audio
-            controls
-            src={sound}
-            type="audio/mpeg"
-          />
-
+      <Button sound={sound}/>
     </li>
   )
 }
