313fe04109956f18ad94f53049e07e4c66dac762
double T <tangy54tondeur69@gmail.com>
2024-10-06 16:34:48 +0200

    nav change / ajout page

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,6 @@ export default async function Home() {
         <h1 className="text-6xl font-black">SOUNDBOX MTF</h1>
         <h2>seulement pour les mecs COOL</h2>
       </header>
-      <div className="px-24">
-        <ul className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
-
-          {post.results.map((i) => (
-            <Card
-              key={i.id}
-              name={i.properties.Nom.title[0].plain_text}
-              image={i.properties.image.files[0]?.file.url}
-              sound={i.properties.son.files[0]?.file.url}
-            />
-          ))}
-
-        </ul>
-      </div>
     </>
   );
 }
