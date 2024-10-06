3662747b8016ac4e6ce52f0729c6f007f720633d
double T <tangy54tondeur69@gmail.com>
2024-10-06 17:08:52 +0200

    h2 change

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <Navigation />
       <header className="h-[600px] w-full azerty flex items-center justify-center flex-col text-3xl  text-white gap-10 ">
         <h1 className="text-6xl font-black">SOUNDBOX MTF</h1>
-        <h2>seulement pour les mecs</h2>
+        <h2>seulement pour les mecs COOL</h2>
       </header>
       <div className="px-24">
         <ul className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
--- app/page3/page.tsx
+++ app/page3/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
     <>
       <Navigation />
       <header className="h-[600px] w-full azerty flex items-center justify-center flex-col text-3xl  text-white gap-10 ">
-        <h1 className="text-6xl font-black">SOUNDBOX</h1>
+        <h1 className="text-6xl font-black">SOUNDBOX MTF</h1>
         <h2>seulement pour les mecs COOL</h2>
       </header>
       <div className="px-24">
